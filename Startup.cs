b93b74583fdce454175e9e438643923c504cb71d
grusnny <37426267+grusnny@users.noreply.github.com>
2021-02-21 18:31:42 -0500

    Add docker-compose

--- Startup.cs
+++ Startup.cs
@@ -1,18 +1,10 @@
 using Clothesstore.Context;
-using Clothesstore.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Clothesstore
 {
@@ -32,7 +24,7 @@ namespace Clothesstore
 
             //context and connection to data base
 
-            services.AddDbContext<ProductsContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ProductsContext>(opts => opts.UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
 
             services.AddControllers();
         }
