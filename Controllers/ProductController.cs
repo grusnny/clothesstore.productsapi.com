b561121dc82c423b88389f8230a1260367c0f55e
grusnny <37426267+grusnny@users.noreply.github.com>
2021-02-21 13:01:31 -0500

    Add docker-compose

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Clothesstore.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ProductController : ControllerBase
     {
